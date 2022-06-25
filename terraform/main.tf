9ab3e46ef234f5d8a88291acb7b946a98d022967
Pierre-Olivier Rey <pierre-olivier.rey@epita.fr>
2022-06-25 19:40:59 +0200

    terraform: add k8s nodes

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.10"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "https://192.168.1.100:8006/api2/json"
+
+  pm_tls_insecure = true
+  pm_debug = true
+}
